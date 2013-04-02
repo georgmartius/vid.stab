37b7c41930fcad722770c8829252265b3e1d8571
Georg Martius <georg.martius@web.de>
2013-04-02 14:26:29 +0200

    transcode install script

--- transcode/cmake/install.sh
+++ transcode/cmake/install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-PATHS=/usr/lib/transcode /usr/local/lib/transcode $HOME/lib/transcode
+PATHS="/usr/lib/transcode /usr/local/lib/transcode $HOME/lib/transcode"
 for D in $PATHS; do
     if [ -d $D ]; then
         echo -n "Do you want to install the plugins to $D ? [y/N] ";
         read choice
         if [ "$choice" = "y" ]; then
-            echo  "Checking PATH variable: "	
-            SUDO=
-            if [ ! -w $(PREFIX) ]; then SUDO=sudo; fi
-	    $(SUDO) cp --backup=numbered ./filter_stabilize.so ./filter_transform.so ./filter_deshake.so "$D/"
+            echo -n "Copy files... "	
+            SUDO=;
+            if [ ! -w $D ]; then SUDO=sudo; echo " (need root permission, so I use sudo) "; fi
+	    $SUDO cp -v --backup=numbered ./filter_stabilize.so ./filter_transform.so ./filter_deshake.so "$D/" && echo "done";
         fi
     fi
 done

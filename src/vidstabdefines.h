cf027ae566c9f719c5c2bbc9949a223c74738157
Clément Bœsch <clement@stupeflix.com>
2013-09-13 10:10:24 +0200

    defines: simplify PIXEL() and PIXELN()

--- src/vidstabdefines.h
+++ src/vidstabdefines.h
@@ -37,13 +37,13 @@
 #define VS_DEBUG 2
 
 /// pixel in single layer image
-#define PIXEL(img, linesize, x, y, w, h, def) ((x) < 0 || (y) < 0) ? (def)  \
-  : (((x) >= (w) || (y) >= (h)) ? (def) : img[(x) + (y) * (linesize)])
+#define PIXEL(img, linesize, x, y, w, h, def) \
+  (((x) < 0 || (y) < 0 || (x) >= (w) || (y) >= (h)) ? (def) : img[(x) + (y) * (linesize)])
 /// pixel in single layer image without rangecheck
 #define PIX(img, linesize, x, y) (img[(x) + (y) * (linesize)])
 /// pixel in N-channel image. channel in {0..N-1}
-#define PIXELN(img, linesize, x, y, w, h, N, channel, def) ((x) < 0 || (y) < 0) ? (def) \
-  : (((x) >=(w) || (y) >= (h)) ? (def) : img[((x) + (y) * (linesize))*(N) + (channel)])
+#define PIXELN(img, linesize, x, y, w, h, N, channel, def) \
+  (((x) < 0 || (y) < 0 || (x) >= (w) || (y) >= (h)) ? (def) : img[((x) + (y) * (linesize))*(N) + (channel)])
 /// pixel in N-channel image without rangecheck. channel in {0..N-1}
 #define PIXN(img, linesize, x, y, N, channel) (img[((x) + (y) * (linesize))*(N) + (channel)])
 

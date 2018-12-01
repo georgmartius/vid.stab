f29e9213c92602c76a81700596d9a72aedec9572
Jonathan Noble <jonnobleuk@gmail.com>
2018-12-01 15:36:21 +0000

    perormance testing scripts update

--- benchmarks/transform.sh
+++ benchmarks/transform.sh
@@ -3,5 +3,6 @@
 
 TEST_FILE="../samples/test001.mp4"
 OUT_FILE="stabilised.mp4"
+#OPTS="=interpol=bicubic"
 rm -fv ${OUT_FILE}
-time ffmpeg -i ${TEST_FILE} -vf vidstabtransform ${OUT_FILE}
+time ffmpeg -i ${TEST_FILE} -vf vidstabtransform${OPTS} ${OUT_FILE}
--- profiling/profiler.sh
+++ profiling/profiler.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+set -x
 
 TEST_FILE="../samples/test001.mp4"
 OUT_FILE="stabilised.mp4"
-
-rm -fv *.data *.trf *.mp4
+#T_OPTS="=interpol=bicubic"
+rm -fv ${OUT_FILE}
+rm -fv *.data *.trf
 perf record -o detect.data ffmpeg -i ${TEST_FILE} -vf vidstabdetect=${OPTS} -f null -
-perf record -o transform.data ffmpeg -i ${TEST_FILE} -vf vidstabtransform ${OUT_FILE}
+#perf record -o transform.data ffmpeg -i ${TEST_FILE} -vf vidstabtransform${T_OPTS} ${OUT_FILE}
+perf record -o transform.data ffmpeg -i ${TEST_FILE} -vf vidstabtransform${T_OPTS} -f null -   # ~6% speedup

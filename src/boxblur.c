8a1f84f8deddfcaa9b1c6cf14dd499b71dad8bca
Georg Martius <martius@mis.mpg.de>
2026-08-06 08:25:59 +0200

    Relicense the library under LGPL-2.1-or-later
    vid.stab is a library, and under the GPL every application linking it had
    to be GPL as well.  In practice that meant ffmpeg could only build the
    vidstabdetect and vidstabtransform filters under --enable-gpl, so most
    distributed ffmpeg builds shipped with no video stabilization at all.
    
    Under the LGPL an application may link vid.stab without taking on its
    licence, while modifications to vid.stab itself still have to be
    published under the LGPL -- the reciprocity that matters for a library.
    
    Every contributor whose code is in src/ agreed to the change in issue
    #165; the statements are recorded in RELICENSE.md.  Permission was
    granted in addition to the GPL, so copies obtained under the GPL keep
    their GPL terms.
    
    Only comment blocks change here, and an SPDX-License-Identifier line is
    added to each file.  Five files still described themselves as part of
    transcode, where they were written many years ago; they say vid.stab now.

--- src/boxblur.c
+++ src/boxblur.c
@@ -4,22 +4,23 @@
  *  Copyright (C) Georg Martius - July 2010
  *   georg dot martius at web dot de
  *
+ *  SPDX-License-Identifier: LGPL-2.1-or-later
+ *
  *  This file is part of vid.stab video stabilization library
  *
  *  vid.stab is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License,
- *  as published by the Free Software Foundation; either version 2, or
+ *  it under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation; either version 2.1 of the License, or
  *  (at your option) any later version.
  *
  *  vid.stab is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *  GNU Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with GNU Make; see the file COPYING.  If not, write to
- *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- *  Boston, MA 02110-1301, USA.
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with vid.stab; see the file COPYING.LESSER.  If not, see
+ *  <https://www.gnu.org/licenses/>.
  *
  */
 
